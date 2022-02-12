6afd37f9352939dd887c3b951c3b406572ffc38b
Aiman <55715766+Danieloch14@users.noreply.github.com>
2022-02-12 14:52:03 -0500

    Update ConsultarMultasUsuarioController.java
    Correcion Servlet consultar multas contoller

--- project_mvc_multas/src/main/java/controlador/ConsultarMultasUsuarioController.java
+++ project_mvc_multas/src/main/java/controlador/ConsultarMultasUsuarioController.java
@@ -20,7 +20,7 @@ import modelo.entidades.Vehiculo;
 /**
  * Servlet implementation class ConsultarMutasUsuarioController
  */
-@WebServlet("/ConsultarMutasUsuarioController")
+@WebServlet("/ConsultarMultasUsuarioController")
 public class ConsultarMultasUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
